c5c77b430bf84702d3cbafc670f134504c2bc5bc
zanerosu <zanerosu@gmail.com>
2023-11-19 21:14:22 -0500

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "conference paper system"
+})
+
+
+app.listen(8081, ()=> {
+    console.log("listening");
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM `users` WHERE `Username` = ? AND `Password` = ?";
+    db.query(sql, [req.body.username, req.body.password], (err, data) => {
+        if(err){
+            return res.json("Error");
+        }
+        if(data.length > 0){
+            return res.json("Success");
+        } else{
+            return res.json("Fail");
+        }
+    })
+})
